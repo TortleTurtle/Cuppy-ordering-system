5a6181bc7cfd6986511d3144ba04f130f67d66c9
TortleTurtle <danielmeijs@gmail.com>
2019-11-19 14:23:38 +0100

    set fillable collumns in models

--- app/Cup.php
+++ app/Cup.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cup extends Model
 {
+    protected $fillable = ['created_at'];
+
     //relationships
     public function owner(){
         return $this->belongsTo('App\User', 'owner');
--- app/Order.php
+++ app/Order.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $fillable = [
+        'clip', 'engrave', 'front_img', 'back_img', 'location'
+    ];
+
     //relationships
     public function owner(){
         return $this->belongsTo('App\User', 'owner');
